abfa03700ee1a747948765227afdfdaf8e1e5a35
GitHub Action <action@github.com>
2021-11-18 11:29:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/inhere/ideplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/inhere/ideplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.inhere.ideplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.inhere.ideplugindemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/inhere/ideplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/inhere/ideplugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.inhere.ideplugindemo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.inhere.ideplugindemo.MyBundle
 
 class MyProjectService(project: Project) {
 
